78b8fc3d8a8f9f20cc386e30859f890ca379b3cc
AnthonyCTN <135627741+AnthonyCTN@users.noreply.github.com>
2024-06-07 14:03:52 +0200

    Add files via upload

---
+++ chatfr/amis.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : jeu. 06 juin 2024 à 09:16
+-- Version du serveur : 5.7.39
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `chatfr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `amis`
+--
+
+CREATE TABLE `amis` (
+  `id_utilisateur1` int(11) NOT NULL,
+  `id_utilisateur2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `amis`
+--
+
+INSERT INTO `amis` (`id_utilisateur1`, `id_utilisateur2`) VALUES
+(2, 1),
+(4, 1),
+(2, 2),
+(1, 4);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `amis`
+--
+ALTER TABLE `amis`
+  ADD PRIMARY KEY (`id_utilisateur1`,`id_utilisateur2`),
+  ADD KEY `id_utilisateur2` (`id_utilisateur2`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `amis`
+--
+ALTER TABLE `amis`
+  ADD CONSTRAINT `amis_ibfk_1` FOREIGN KEY (`id_utilisateur1`) REFERENCES `utilisateurs` (`id`),
+  ADD CONSTRAINT `amis_ibfk_2` FOREIGN KEY (`id_utilisateur2`) REFERENCES `utilisateurs` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
